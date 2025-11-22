c95d0b8867cbe9f7e88a53359324d622e59a7ec7
Wildstar7 <catswildstarjapan7@gmail.com>
2025-11-22 15:53:25 -0800

    Updated all references to Eugene to Springfield

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -11,14 +11,17 @@ async function loadBusinessData() {
         const response = await fetch('data/members.json', { cache: "no-store" });
         if (!resp.ok) throw new Error('Error getting members.json');
         businessData = await resp.json();
-        renderBusiness();
+        renderBusinessCards(businessData);
     } catch (err) {
         businessContainer.innerHTML = `<p class="error"> Couldn't find business list. ${err.message}</p>`;
         console.error(err);
     }
 }
 
-const renderBusinessCards = (businesses) => {
+const renderBusinessCards = (businessData) => {
     // build code
-    
+    businessData.forEach((business) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement()
+    })
 }
